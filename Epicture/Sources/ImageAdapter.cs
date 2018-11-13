bc993b6a1759c301614e6d683fc5ba2ee143c045
Robin Chatain <robin.chatain@epitech.eu>
2018-11-13 17:12:49 +0100

    [Add] imageAdapter

---
+++ Epicture/Sources/ImageAdapter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V4.View;
+using Android.Views;
+using Android.Widget;
+using Java.Lang;
+
+namespace Epicture.Sources
+{
+    public class ImageAdapter : PagerAdapter
+    {
+        private Context context;
+
+        public ImageAdapter(Context context)
+        {
+            this.context = context;
+        }
+
+        public override int Count
+        {
+            get
+            {
+                return imageList.Length;
+            }
+        }
+
+        public override bool IsViewFromObject(View view, Java.Lang.Object @object)
+        {
+            return view == ((ImageView)objectValue);
+        }
+
+        public override Java.Lang.Object InstantiateItem(View container, int position)
+        {
+            ImageView imageView = new ImageView(context);
+            imageView.SetScaleType(ImageView.ScaleType.CenterCrop);
+            imageView.SetImageResource(imageList[position]);
+            ((ViewPager)container).AddView(imageView, 0);
+        }
+
+        public override void DestroyItem(View container, int position, Java.Lang.Object @object)
+        {
+            ((ViewPager)container).RemoveView((ImageView)objectValue);
+        }
+    }
+}
