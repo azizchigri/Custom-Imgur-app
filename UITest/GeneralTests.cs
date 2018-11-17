ef930c7fe84439352f61b01243668d518cab61aa
Aziz Chigri <aziz.chigri@epitech.eu>
2018-11-18 00:15:22 +0100

    [Unit test] UI tests are finished

---
+++ UITest/GeneralTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace UITest
+{
+
+    [TestFixture(Platform.Android)]
+    public class GeneralTests
+    {
+        IApp app;
+        Platform platform;
+
+        public GeneralTests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [OneTimeSetUp]
+        public void BeforeFirstTest()
+        {
+            app = AppInitializer.app;
+            app.WaitForElement(marked: "Open navigation drawer", timeout: TimeSpan.FromMinutes(1));
+            app.Tap("Open navigation drawer");
+        }
+
+        [Test]
+        public void CheckUserInfo()
+        {
+            AppResult[] results = app.Query(c => c.Marked("xamarin@microsoft.com"));
+            Assert.IsTrue(results.Count() == 0);
+            results = app.Query(c => c.Marked("Xamarin.Android"));
+            Assert.IsTrue(results.Count() == 0);
+        }
+
+        [Test]
+        public void Disconnect()
+        {
+            app.Tap("Search");
+        }
+    }
+}
